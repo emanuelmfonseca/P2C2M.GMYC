9f7d9f8094e419f6e24799a49fe64ee450b7872a
Emanuel <emanuelmfonseca@gmail.com>
2020-05-03 10:51:31 -0400

    New version - May 3

--- R/simulated.tree.R
+++ R/simulated.tree.R
@@ -1,7 +1,7 @@
-GMYC.simulated.tree <- function(Yule.tree, Species.result){
-  
-  if(length(Species.result[,1]) > 1){
-  
+GMYC.simulated.tree <- function(Yule.tree, Species.result, n.tips){
+
+  if(length(Species.result[,1]) != n.tips & length(Species.result[,1]) != 1){
+
   number = 0
   for (u in 1:length(Species.result[,1])){
     if (Species.result[u,2] > 1){
@@ -10,11 +10,9 @@ GMYC.simulated.tree <- function(Yule.tree, Species.result){
         sim <- sim.coaltree(nspecies=Species.result[u,2],theta=Species.result[u,3])
         sim <-read.tree(text=paste(sim,";",sep=""))
         sim$tip.label<-paste(rep(paste("Tip", Species.result[u,1], ".", sep=""), e=Species.result[u,2]), 1:Species.result[u,2], sep="")
-        
+
         if (any(Species.result[,1] > 1)){
           Grafted.tree <- bind.tree(Yule.tree, sim, where=which(Yule.tree$tip.label == paste("Tip", u, ".1", sep="")))
-        } else {
-          Grafted.tree <- sim
         }} else {
           sim <- sim.coaltree(nspecies=Species.result[u,2],theta=Species.result[u,3])
           sim<-read.tree(text=paste(sim,";",sep=""))
@@ -23,10 +21,15 @@ GMYC.simulated.tree <- function(Yule.tree, Species.result){
           Grafted.tree <- force.ultrametric(Grafted.tree, method="nnls")
           Grafted.tree$edge.length<- Grafted.tree$edge.length/max(nodeHeights(Grafted.tree)[,2])
         }}}
-  } else {
+  } else if (length(Species.result[,1]) == n.tips){
     Grafted.tree <- Yule.tree
+  } else {
+    sim <- sim.coaltree(nspecies=Species.result[1,2],theta=Species.result[1,3])
+    sim <-read.tree(text=paste(sim,";",sep=""))
+    sim$tip.label<-paste(rep(paste("Tip", Species.result[1,1], ".", sep=""), e=Species.result[1,2]), 1:Species.result[1,2], sep="")
+    Grafted.tree <- sim
   }
-  
+
   return (Grafted.tree)
-  
+
 }
