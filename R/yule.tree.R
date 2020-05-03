120fa8c98984a0692659d23ce561b0fb26c2e883
Emanuel <emanuelmfonseca@gmail.com>
2020-05-03 11:01:21 -0400

    New version - May 3

--- R/yule.tree.R
+++ R/yule.tree.R
@@ -19,10 +19,4 @@ Yule.tree <- function(Species.result, empirical.tree){
 
     return(NULL)
 
-    # yule.tree<-sim.bd.taxa(n=Species.result[1,2], numbsim=1, lambda=yule_lambda, mu=0, frac = 1, complete = TRUE, stochsampling = FALSE)[[1]]
-    # yule.tree$tip.label<-paste(rep(paste("Tip", 1:Species.result[,2], ".1", sep="")))
-    # yule.tree$tip.label<-sample(yule.tree$tip.label)
-    # yule.tree$tip.label <- sample(yule.tree$tip.label)
-    # yule.tree$edge.length[yule.tree$edge[,2] <= Ntip(yule.tree)]<- yule.tree$edge.length[yule.tree$edge[,2] <= Ntip(yule.tree)] + 0.05
-    # yule.tree$edge.length<- yule.tree$edge.length/max(nodeHeights(yule.tree)[,2])
   }}
